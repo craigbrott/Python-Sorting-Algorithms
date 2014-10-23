b7e20e032c108ad60f59f786e88248f35e3b8616
Craig Brott <craigbrott@gmail.com>
2014-10-23 14:08:00 -0700

    Added real comments, reorganized sort.py, removed operation counters, and updates to readme.

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-Python-Sorting-Algorithms
-=========================
+# Sorting Algorithms in Python
 
-Python implementations of Insertion Sort, Quick Sort, and Merge Sort
+Python implementations of Insertion Sort, Quick Sort, and Merge Sort.
 
-Implemented for Algorithm Design & Analysis class.
+----------------------------------
+
+## How to format input files
+
+You're free to use the functions on their own, but if you want to use the built-in file reading functionality just put the values (each on a new line) in a file within the `input` directory.
+
+### E.g.
+
+```
+20401
+11087
+62176
+70095
+20947
+20098
+90914
+53475
+51251
+20065
+```
--- sort.py
+++ sort.py
@@ -1,263 +1,240 @@
+#!/usr/bin/env python
+
 import glob
 import sys
 import math
 import copy
 
-#need to increase the system's recursion limit for Quick Sort
-sys.setrecursionlimit(10000)
-
-## read in data files
-
-#generate list of files
-fileList = glob.glob("./input/*")
-
-#need three copies of the data for the three different sorting algorithms
-fileDataInsertion = []
-fileDataQuick = []
-fileDataMerge = []
-
-for file in fileList:
-    #open file for reading
-    data = open(file, "r")
+'''
+    Insertion Sort.
     
-    #store the data in temp as an array
-    temp = []
-    for element in data:
-        temp.append(int(element))
-    
-    #append the temp array to fileData (deep copy to ensure we don't have references)
-    fileDataInsertion.append(copy.deepcopy(temp))
-    fileDataQuick.append(copy.deepcopy(temp))
-    fileDataMerge.append(copy.deepcopy(temp))
-
-## define sorting algorithms
-
-## Insertion Sort
-
-#Insertion Sort
-def insertionSort(data):
-    #get the global counters ready
-    global comparisonsI
-    global operationsI
-    
-    #traverse the entire array
+    Parameters
+    ----------
+    data: list
+        List of values to be sorted.
+        
+    Returns
+    -------
+    sorted list
+'''
+def insertion_sort(data):
+    # traverse the entire array
     for i in range(1, len(data)):
-        operationsI += 1
-        
-        #store the key to be used in the following while loop
         key = data[i]
-        operationsI += 1
-        
-        #counter j is initialized to the value of i
         j = i
-        operationsI += 1
         
-        #traverse the sorted portion of the array looking for elements that are smaller than or equal to the key
-        comparisonsI += 1
-
-        #need a this comparison flag to check if the while loop triggered
-        comparisonFlag = False
+        # traverse the sorted portion of the array looking for elements that are smaller than or equal to the key
         while(j > 0 and key <= data[j-1]):
-            comparisonFlag = True
-            comparisonsI += 1
-            operationsI += 2
-            
-            #move element j-1 to the right to make room for the key
+            # move element j - 1 to the right to make room for the key
             data[j] = data[j - 1]
-            operationsI += 2
-            
             j -= 1
-            operationsI += 1
             
-        if comparisonFlag:
-            comparisonsI -= 1
-            
-        #insert key in the appropriate place
+        # insert key in the appropriate place
         data[j] = key
-        operationsI += 1
+        
     return data
     
-## Quick Sort
-    
-#Quick Sort
-def quickSort(data):
-    #get the global counters ready
-    global comparisonsQ
-    global operationsQ
-    
-    #recursive case: array size greater than 1
-    operationsQ += 2
+'''
+    Quick Sort.
+
+    Parameters
+    ----------
+    data: list
+        List of values to be sorted.
+        
+    Returns
+    -------
+    sorted list
+'''
+def quick_sort(data):    
+    # recursive case: array size greater than 1
     if(len(data) > 1):
-        #calculate the pivot by partitioning
+        # calculate the pivot by partitioning
         pivot = partition(data)
-        operationsQ += 1
         
-        #omit the pivot and recursively call quickSort()
-        left = quickSort(data[:pivot])
-        right = quickSort(data[pivot+1:])
-        operationsQ += 5
+        # omit the pivot and recursively call quick_sort()
+        left = quick_sort(data[:pivot])
+        right = quick_sort(data[pivot+1:])
         
-        #combine the left and right partitions along with the pivot
+        # combine the left and right partitions along with the pivot
         left.append(data[pivot])
         left.extend(right)
-        operationsQ += 2
         
         return left
-    #base case: return array of size 1
+    # base case: return array of size 1
     else:
         return data
 
-#Partition
+'''
+    This is the partition step of Quick Sort.
+
+    Parameters
+    ----------
+    data: list
+        List of values for which we want to determine the pivot value.
+        
+    Returns
+    -------
+    int
+'''
 def partition(data):
-    #get the global counters ready
-    global comparisonsQ
-    global operationsQ
-    
-    #set the pivot to be the element at the start index
+    # set the pivot to be the element at the start index
     pivot = data[0]
     h = 0
-    operationsQ += 2
     
-    #start k at 1 and switch values that are less than the pivot
+    # start k at 1 and switch values that are less than the pivot
     for k in range(1, len(data)):
-        comparisonsQ += 1
-        operationsQ += 1
         if(data[k] < pivot):
             h += 1
-            operationsQ += 1
-            
             swap(data, h, k)
-    #put the pivot in its new position
+
+    # put the pivot in its new position
     swap(data, 0, h)
     
-    #return the new position of the pivot
+    # return the new position of the pivot
     return h
 
-#Swap
-def swap(data, i, j):
-    #get the global counters ready
-    global comparisonsQ
-    global operationsQ
+'''
+    This is the swap step of Quick Sort.
     
+    Parameters
+    ----------
+    data: list
+        The list we're sorting.
+    i: int
+        The index of the first value.
+    j: int
+        The index of the second value.
+'''
+def swap(data, i, j):
     temp = data[i]
     data[i] = data[j]
     data[j] = temp
-    operationsQ += 3
     
-## Merge Sort
-
-#Merge Sort
-def mergeSort(data):
-    #get the global counters ready
-    global comparisonsM
-    global operationsM
+'''
+    Merge Sort.
     
-    #store the size of the array passed into mergeSort for later use (reduces operation count)
-    dataLen = len(data)
-    operationsM += 1
+    Parameters
+    ----------
+    data: list
+        List of values to be sorted.
+        
+    Returns
+    ----------
+    sorted list
+'''
+def merge_sort(data):
+    # store the size of the array passed into merge_sort for later use (reduces operation count)
+    data_len = len(data)
     
-    #recursive case: array size greater than 1
-    operationsM += 1
-    if(dataLen > 1):
+    # recursive case: array size greater than 1
+    if(data_len > 1):
         start = 0
-        operationsM += 1
         
-        #find the midpoint
-        middle = int(math.floor(dataLen / 2))
-        operationsM += 3
+        # find the midpoint
+        middle = int(math.floor(data_len / 2))
         
-        #split the array into two halves and recursively call mergeSort()
-        left = mergeSort(data[:middle])
-        right = mergeSort(data[middle:])
-        operationsM += 2
+        # split the array into two halves and recursively call merge_sort()
+        left = merge_sort(data[:middle])
+        right = merge_sort(data[middle:])
         
-        #finally, merge the sorted lists and return the result
+        # finally, merge the sorted lists and return the result
         return merge(left, right)
-    #base case: return an array of one element
+    # base case: return an array of one element
     else:
         return data
 
-#Merge
+'''
+    This is the merge step of Merge Sort.
+    
+    Parameters
+    ----------
+    left: list
+        The left half of the list, minus the pivot.
+    right: list
+        The right half of the list, minus the pivot.
+    
+    Returns
+    ----------
+    list
+'''
 def merge(left, right):
-    #get the global counters ready
-    global comparisonsM
-    global operationsM
-    
-    #temporary array to hold the sorted result
+    # temporary array to hold the sorted result
     result = []
-    operationsM += 1
     
-    #while loop that cycles through the left and right halves fully and sorts one by one
+    # while loop that cycles through the left and right halves fully and sorts one by one
     while(len(left) > 0 or len(right) > 0):
-        operationsM += 2
-        #if there are elements left in both the left and right halves, find out which element is greater and append the lesser element
-        operationsM += 2
+        # if there are elements left in both the left and right halves, find out which element is greater and append the lesser element
         if(len(left) > 0 and len(right) > 0):
-            comparisonsM += 1
             if(left[0] <= right[0]):
-                operationsM += 1
-
                 result.append(left[0])
                 left = left[1:]
-                operationsM += 2
             else:
                 result.append(right[0])
                 right = right[1:]
-                operationsM += 2
-        #if there is an element left in only the left half, copy it to the result and delete that element from the left subarray
+        # if there is an element left in only the left half, copy it to the result and delete that element from the left subarray
         elif(len(left) > 0):
-            operationsM += 1
-
             result.append(left[0])
             left = left[1:]
-            operationsM += 2
-        #if there is an element left in only the right half, copy it to the result and delete that element from the right subarray
+        # if there is an element left in only the right half, copy it to the result and delete that element from the right subarray
         elif(len(right) > 0):
-            operationsM += 1
-            
             result.append(right[0])
             right = right[1:]
-            operationsM += 2
-    #return the merged array
+    # return the merged array
     return result
+
+'''
+    Driver function that gets all the files in the input/ directory, runs each of the three sorting algorithms, and prints the output.
+'''
+def main():
+    # need to increase the system's recursion limit for Quick Sort
+    sys.setrecursionlimit(10000)
     
-## call sorting algorithms for each array
-
-print "##############################"
-print "#       Insertion Sort       #"
-print "##############################\n"
-for data in fileDataInsertion:
-    #set up counter variables for insertion sort
-    comparisonsI = 0
-    operationsI = 0
-    print insertionSort(data)
-    print "input size: ", len(data)
-    print "total comparisons: ", comparisonsI
-    print "total operations: ", operationsI
-    print "\n"
-
-print "##############################"
-print "#         Quick Sort         #"
-print "##############################\n"
-for data in fileDataQuick:
-    #set up counter variables for quick sort
-    comparisonsQ = 0
-    operationsQ = 0
-    print quickSort(data)
-    print "input size: ", len(data)
-    print "total comparisons: ", comparisonsQ
-    print "total operations: ", operationsQ
-    print "\n"
-
-print "##############################"
-print "#         Merge Sort         #"
-print "##############################\n"
-for data in fileDataMerge:
-    #set up counter variables for merge sort
-    comparisonsM = 0
-    operationsM = 0
-    print mergeSort(data)
-    print "input size: ", len(data)
-    print "total comparisons: ", comparisonsM
-    print "total operations: ", operationsM
-    print "\n"
+    # generate list of input files
+    file_list = glob.glob('./input/*')
+    
+    # need three copies of the data for the three different sorting algorithms
+    insertion_raw_data = []
+    quick_raw_data = []
+    merge_raw_data = []
+    
+    for file in file_list:
+        # open file for reading
+        data = open(file, 'r')
+        
+        # store each element in the temp list
+        temp = []
+        for element in data:
+            temp.append(int(element))
+            
+        # deep copy to ensure we don't have references
+        insertion_raw_data.append(copy.deepcopy(temp))
+        quick_raw_data.append(copy.deepcopy(temp))
+        merge_raw_data.append(copy.deepcopy(temp))
+        
+    print '##############################'
+    print '#       Insertion Sort       #'
+    print '##############################\n'
+    for data in insertion_raw_data:
+        print 'Input size: {}'.format(len(data))
+        print insertion_sort(data)
+        print ''
+        
+    print '##############################'
+    print '#         Quick Sort         #'
+    print '##############################\n'
+    for data in quick_raw_data:
+        print 'Input size: {}'.format(len(data))
+        print quick_sort(data)
+        print ''
+        
+    print '##############################'
+    print '#         Merge Sort         #'
+    print '##############################\n'
+    for data in merge_raw_data:
+        print 'Input size: {}'.format(len(data))
+        print merge_sort(data)
+        print ''
+    
+if __name__ == '__main__':
+    main()
